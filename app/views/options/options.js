a90f7b80f99e63da975cd549c010493de72ae5ba
Neli <neli.chakarova@yahoo.com>
2015-09-08 10:48:27 +0300

    Second commit

--- app/views/options/options.js
+++ app/views/options/options.js
@@ -3,6 +3,8 @@ var frameModule = require("ui/frame");
 exports.startQuiz=function(){
 	frameModule.topmost().navigate("./views/quiz/question");
 }
+ 
+
 
 exports.openGallery = function(){
 	frameModule.topmost().navigate("./views/gallery/gallery");
